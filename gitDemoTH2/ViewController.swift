92fc77d79d014d7f40ddecba0f0649646727762b
Der-Beni22 <benjamin.todt@web.de>
2017-11-04 20:37:59 +0100

    Initial Commit

---
+++ gitDemoTH2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH2
+//
+//  Created by Beni on 11/4/17.
+//  Copyright © 2017 Beni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
